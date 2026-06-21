2c1b46b60dbf2e91ae50bb13cbe32fe43ddcce5b
Troy Ong <danieltroy092@gmail.com>
2026-06-21 13:00:32 +0100

    added style modules for nav components

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,9 +1,10 @@
 import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.navlist}>
         <li>
           <NavLink to="/">Homepage</NavLink>
         </li>
