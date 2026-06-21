39c6982dc50fb0327b48606c64a15322ef496f8e
Troy Ong <danieltroy092@gmail.com>
2026-06-21 13:06:42 +0100

    added starter files for page components

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,13 +1,7 @@
-import PageNav from "../components/PageNav";
-
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      {" "}
-      <PageNav />
-      404
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;
